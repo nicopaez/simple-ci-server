b7b0487c1c84de5c3bdff1f8b1448090caea0364
Nicolas Paez <nicopaez@gmail.com>
2023-09-27 12:15:21 -0300

    Update run_ci_server.sh

--- run_ci_server.sh
+++ run_ci_server.sh
@@ -1,9 +1,30 @@
+#!/bin/bash
 
+REPO_URL="https://github.com/......"
+PROJECT_NAME="myproject"
+BUILD_SCRIPT="build.sh"
+API_KEY="some-secret-key"
+DOMAIN="mycompany.com"
 
-curl -s --user 'api:YOUR_API_KEY' \
-    https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages \
-    -F from='Excited User <mailgun@YOUR_DOMAIN_NAME>' \
-    -F to=YOU@YOUR_DOMAIN_NAME \
-    -F to=bar@example.com \
-    -F subject='Hello' \
-    -F text='Testing some Mailgun awesomeness!'
+while true
+do
+    echo "running..."
+    git clone $REPO_URL $PROJECT_NAME
+    cd $PROJECT_NAME
+    bash $BUILD_SCRIPT
+    if [ $? == 0 ]
+    then
+        echo ">>>> build success"
+    else
+        echo ">>>> build FAILED!!!"
+        curl -s --user 'api:API_KEY' \
+        https://api.mailgun.net/v3/DOMAIN/messages \
+        -F from='CI SERVER <no-reply@somedomain.com>' \
+        -F to=TEAM_MAIL \
+        -F subject='[CI] BUILD FAILED!' \
+        -F text='Your build has failed'
+    fi
+    cd ..
+    rm -f -R $PROJECT_NAME
+    sleep 120
+done
