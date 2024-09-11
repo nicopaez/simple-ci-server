45c4024c438f14ae716849171add4a9a1c2490a7
Nicolas Paez <nicopaez@gmail.com>
2024-09-11 10:14:37 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+set -x
+
 echo "building...."
+
+echo "verifying coding conventions"
+rubocop
+
+echo "running unit tests"
+rspec --format progress --format RspecJunitReporter
+
+echo "running acceptante tests"
+cucumber features --format html --tag not @wip
+
